c02acc72a5dfbb744450e262e800b06a400a314a
Matheus Rezende <matheusreisrezende.2002@gmail.com>
2022-10-24 12:44:02 -0300

    Versão Final - Sem Bônus

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const lista = [ "./img/gifs/bobrossparrot.gif",
                 "./img/gifs/explodyparrot.gif", 
                 "./img/gifs/fiestaparrot.gif", 
                 "./img/gifs/metalparrot.gif", 
-                "./img/gifs/reveritparrot.gif", 
+                "./img/gifs/revertitparrot.gif", 
                 "./img/gifs/tripletsparrot.gif", 
-                "./img/gifs/unicornparrot.gif]"];
+                "./img/gifs/unicornparrot.gif"];
 let randlista = [];
 let selectedCard = 0;
-let viradas = []
+let jogadas = 0;
+let viradas;
+let man0; let man1;
 function DistribuiCartas(){
     let qtd = prompt("Com quantas cartas você quer jogar? Escolha um número par de 4 à 14.");
     while(qtd%2 !== 0 || qtd < 4 || qtd > 14){
@@ -28,15 +30,18 @@ function DistribuiCartas(){
         mesa.innerHTML += `<div onclick="viraCarta(this)" class="card-back"><img class="parrot not-display" src=${randlista[i]}><img class="backimg" src="./img/back.png" alt=""></div>`;
     } //Coloca na mesa as cartas com as imagens embaralhadas
 
-    console.log("Passou")
+}
+function toggle(elemento) {
+    elemento.querySelector(".backimg").classList.toggle("not-display");
+    setTimeout(elemento.querySelector(".parrot").classList.toggle("not-display"), 500);
 }
 function manipulaImagem(elemento){
-    elemento.classList.toggle("virada")
-    setInterval(elemento.querySelector(".backimg").classList.toggle("not-display"),1000)
-    elemento.querySelector(".parrot").classList.toggle("not-display")
-}
+    elemento.classList.toggle("virada");
+    setTimeout(toggle, 500, elemento);
+}   
 function viraCarta(elemento){
-    if(elemento.classList.contains("virada") === true){
+    jogadas++
+    if(elemento.classList.contains("virada") === true || elemento.classList.contains("acertou") === true){
         alert("Esta carta já está virada.")
     }
     else{
@@ -52,30 +57,33 @@ function viraCarta(elemento){
     }
     
 }
+function manipula(){
+    viradas = document.querySelectorAll('.virada')
+    manipulaImagem(viradas[0]);
+    manipulaImagem(viradas[1]);
+}
+
 function verificaCarta(){
-    console.log("executou")
     viradas = document.querySelectorAll('.virada');
-    if(viradas[1].innerHTML === viradas[0].innerHTML){
+    if(viradas[1].querySelector(".parrot").src === viradas[0].querySelector(".parrot").src){
         viradas[0].classList.remove("virada");
         viradas[1].classList.remove("virada");
         viradas[0].classList.add("acertou");
         viradas[1].classList.add("acertou");
     }
-    else{
-        manipulaImagem(viradas[0])
-        manipulaImagem(viradas[1])
-        viradas = []
+    else{  
+        setTimeout(manipula, 1500);
+        viradas = [];
+    }
+    setTimeout(fimJogo, 1500);
+}
+function fimJogo(){
+    let container = document.querySelector(".card-container")
+    if(container.querySelectorAll(".acertou").length === container.querySelectorAll(".card-back").length){
+       alert(`Você ganhou em ${jogadas} jogadas!`) 
     }
 }
-// function verificaCarta(elemento){
-//     const carta1 = selectedCard[0]
-//     const carta2 = selectedCard[1]
-//     if(carta1.querySelector(".parrot").innerHTML !== carta2.querySelector(".parrot").innerHTML){
-//         carta1.classList.remove("virada");
-//         carta2.classList.remove("virada"); 
-//     }
-    
-// }
+
 DistribuiCartas()
 function comparador() { 
 	return Math.random() - 0.5; 
